918822375352e5f04c333bae5b5d68bcb0d722c1
Derbin Artur <ukropsan00@yandex.ru>
2021-06-06 17:52:26 +0500

    add recipe interaction

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from modules.parser import CookBookParser
+from modules.interaction import get_shop_list_by_dishes
 
 
 if __name__ == "__main__":
     cook_book = CookBookParser.read_recipes('./recipes.txt')
     for key in cook_book:
-        print(key, cook_book[key])
+        print(key)
+        for ingredient in cook_book[key]:
+            print(f'\t{ingredient}')
+
+    print()
+
+    shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
+    for key in shop_list:
+        print(key, shop_list[key])
+
